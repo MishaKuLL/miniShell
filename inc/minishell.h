296eeac8ec0acc2eaf7ef8665b8c4f6a21bcbb2d
gglenpoi <84961585+MishaKuLL@users.noreply.github.com>
2022-08-30 20:35:20 +0300

    in progress

--- inc/minishell.h
+++ inc/minishell.h
@@ -7,14 +7,58 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/wait.h>
+#include "libft.h"
+#include <signal.h>
+#include <fcntl.h>
+#include <stdbool.h>
 
-typedef struct s_data
+
+# define D_QUOTE '\"'
+# define QUOTE '\''
+
+int g_ret_number;
+
+typedef struct s_struct
 {
-    char	cmd;
-    int     num;
-    char*   str;
-}	t_data;
+    bool	has_flag;
+    int	num_tokens;
+    char	quote;
+    char	*line;
+    char	*line_read;
+    char	*name_file;
+    char	*error_name_file;
+    char	*home;
+    char	**tokens;
+    char	*cmd;
+    int	last_redir;
+    int	is_append;
+    int	out_fd;
+    int	in_fd;
+    int	c;
+    
+}	t_struct;
+
+/*
+** tokenizer.c
+*/
+typedef struct s_token
+{
+	char	*to_print;
+	char	*to_exec;
+	char	quote;
+	char	*end;
+	char	*new;
+	int		i;
+	int		init;
+	int		len;
+	int		posic;
+}			t_token;
+
+void	initialize(t_struct *data);
+void print_prompt2(void);
+void print_prompt1(void);
+void	free_data(t_struct *data, char *errmsg, int errnum);
+char	*read_cmd(char *buf);
 
 
-void	free_data(t_data *data, char *errmsg, int errnum);
 #endif
